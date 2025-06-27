4fea22b955597557fbe6f882ee96adf7274f45e7
mayk0l <maykol0219@gmail.com>
2025-06-27 19:24:33 -0400

    fix(vercel): simplifica vercel.json y actualiza configuración para resolver errores de MIME type

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API de contratos funcionando.");
+});
+
+
 app.post('/generar-contrato', async (req, res) => {
     const { tipo, datos } = req.body;
 
